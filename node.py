cca462386261c36e26b4a44beefcc2d894a14804
Alysondao <2329978700@qq.com>
2024-06-05 14:47:41 +0800

    support more image detect

--- node.py
+++ node.py
@@ -57,7 +57,7 @@ def calculate_file_hash(filename: str, hash_every_n: int = 1):
 
 def plot_boxes_to_image(image_pil, tgt):
     H, W = tgt["size"]
-    results = tgt["results"]
+    result = tgt["result"]
 
     res_mask = []
     res_image = []
@@ -82,44 +82,42 @@ def plot_boxes_to_image(image_pil, tgt):
         "imageHeight": H,
         "imageWidth": W,
     }
+    for item in result:
+        x1,y1,x2,y2 = item['xmin'],item['ymin'],item['xmax'],item['ymax']
+        label = item["label"]
+        threshold = round(item["conf"], 2)
 
-    for result in results:
-        for item in result:
-            x1,y1,x2,y2 = item['xmin'],item['ymin'],item['xmax'],item['ymax']
-            label = item["label"]
-            threshold = round(item["conf"], 2)
+        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+        points = [[x1, y1], [x2, y2]]
 
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-            points = [[x1, y1], [x2, y2]]
+        # Save labelme json
+        shape = {
+            "label": label,
+            "points": points,
+            "group_id": None,
+            "shape_type": "rectangle",
+            "flags": {},
+        }
+        labelme_data["shapes"].append(shape)
 
-            # Save labelme json
-            shape = {
-                "label": label,
-                "points": points,
-                "group_id": None,
-                "shape_type": "rectangle",
-                "flags": {},
-            }
-            labelme_data["shapes"].append(shape)
+        # Change label
+        label = label + ":" + str(threshold)
+        shape["threshold"] = str(threshold)
 
-            # Change label
-            label = label + ":" + str(threshold)
-            shape["threshold"] = str(threshold)
+        # Draw rectangle on the image using PIL
+        draw.rectangle([(x1, y1), (x2, y2)], outline=box_color, width=3)
 
-            # Draw rectangle on the image using PIL
-            draw.rectangle([(x1, y1), (x2, y2)], outline=box_color, width=3)
+        # Draw label on the image using PIL
+        text_size = draw.textsize(label, font=font)
+        label_ymin = max(y1, text_size[1] + 10)
+        draw.rectangle([(x1, y1 - text_size[1] - 10), (x1 + text_size[0], y1)], fill=box_color)
+        draw.text((x1, y1 - text_size[1] - 10), label, font=font, fill=text_color)
 
-            # Draw label on the image using PIL
-            text_size = draw.textsize(label, font=font)
-            label_ymin = max(y1, text_size[1] + 10)
-            draw.rectangle([(x1, y1 - text_size[1] - 10), (x1 + text_size[0], y1)], fill=box_color)
-            draw.text((x1, y1 - text_size[1] - 10), label, font=font, fill=text_color)
-
-            # Draw mask
-            mask = np.zeros((H, W, 1), dtype=np.uint8)
-            cv2.rectangle(mask, (int(x1), int(y1)), (int(x2), int(y2)), (255, 255, 255), -1)
-            mask_tensor = torch.from_numpy(mask).permute(2, 0, 1).float() / 255.0
-            res_mask.append(mask_tensor)
+        # Draw mask
+        mask = np.zeros((H, W, 1), dtype=np.uint8)
+        cv2.rectangle(mask, (int(x1), int(y1)), (int(x2), int(y2)), (255, 255, 255), -1)
+        mask_tensor = torch.from_numpy(mask).permute(2, 0, 1).float() / 255.0
+        res_mask.append(mask_tensor)
 
     if len(res_mask) == 0:
         mask = np.zeros((H, W, 1), dtype=np.uint8)
@@ -136,13 +134,13 @@ def plot_boxes_to_image(image_pil, tgt):
 
     return res_image, res_mask, labelme_data
 
-def OmDet_detect(image_pil, labels, conf_threshold=0.30, nms_threshold=0.5,device="cuda"):
+def OmDet_detect(data, labels, conf_threshold=0.30, nms_threshold=0.5,device="cuda"):
     prompt = "Detect {}.".format(",".join(labels))
     engine = DetEngine(batch_size=1, device=device)
     res = engine.inf_predict(
         "OmDet-Turbo_tiny_SWIN_T",
         task=prompt,
-        data=[image_pil],
+        data=data,
         labels=labels,
         src_type="pil",  # type of the image_paths, "local/tensor/pil/"url"
         conf_threshold=conf_threshold,
@@ -194,19 +192,22 @@ class ApplyOmDet:
     ):
         check_and_download_models()
 
+        data = []
+        labels = get_labels(prompt)
+        for item in image:
+            image_pil = Image.fromarray(np.clip(255.0 * item.cpu().numpy(), 0, 255).astype(np.uint8)).convert("RGB")
+            data.append(image_pil)
+        results = OmDet_detect(data, labels, conf_threshold, nms_threshold, device)
+        
         res_images = []
         res_masks = []
         res_labels = []
-
-        for item in image:
-            image_pil = Image.fromarray(np.clip(255.0 * item.cpu().numpy(), 0, 255).astype(np.uint8)).convert("RGB")
-            labels = get_labels(prompt)
-            results = OmDet_detect(image_pil, labels, conf_threshold, nms_threshold, device)
-
+        
+        for image_pil, result in zip(data, results):
             size = image_pil.size
             pred_dict = {
                 "size": [size[1], size[0]],
-                "results":results
+                "result":result
             }
 
             image_tensor, mask_tensor, labelme_data = plot_boxes_to_image(image_pil, pred_dict)
